5bcaf635256501f1b09a87eb689bced8ad28d959
Nihal <nihalmotukuri@gmail.com>
2025-05-20 00:21:19 +0530

    second commit

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,19 +3,21 @@ import './index.css'
 const Home = () => {
     return (
         <section >
-            <video 
-                className='video-container' 
-                autoPlay 
-                muted 
-                loop 
-                playsInline 
-                disablePictureInPicture 
-                controlsList="nodownload nofullscreen noremoteplayback" >
-                <source 
-                    src="/trailer.mp4"
-                    type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
+            <div className="trailer-container" >
+                <video
+                    className="trailer"
+                    autoPlay 
+                    muted 
+                    loop 
+                    playsInline 
+                    disablePictureInPicture 
+                    controlsList="nodownload nofullscreen noremoteplayback" >
+                    <source 
+                        src="/trailer.mp4"
+                        type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
         </section>
     )
 }
--- src/pages/SearchMovies/index.jsx
+++ src/pages/SearchMovies/index.jsx
@@ -1,9 +1,88 @@
+import { useState, useEffect } from 'react'
+import { FadeLoader } from "react-spinners";
 import './index.css'
 
 const SearchMovies = () => {
-    return (
-        <section>
+    const [searchInput, setSearchInput] = useState('')
+    const [searchResults, setSearchResults] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
 
+    const handleSearch = e => {
+        e.preventDefault()
+        setIsLoading(true)
+
+        const getTrailerKey = async movieId => {
+            const url = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=ab2e9fcb961ed38190348436b4044af8`
+            try {
+                const res = await fetch(url)
+                const jsonData = await res.json()
+                const results = jsonData.results[0]
+                if (results && results.type === 'Trailer') {
+                    return results.key
+                }
+            } catch (err) {
+                console.error(err)
+            }
+        }
+
+        const fetchMovies = async () => {
+            const url = `https://api.themoviedb.org/3/search/movie?api_key=ab2e9fcb961ed38190348436b4044af8&query=${searchInput}`
+            try {
+                const res = await fetch(url)
+                const jsonData = await res.json()
+                const results = jsonData.results
+                const movieDetails = await Promise.all(
+                    results.map(async movie => {
+                        const trailerKey = await getTrailerKey(movie.id)
+                        if (!trailerKey) return null
+                        return {
+                            ...movie,
+                            trailerKey
+                        }
+                }))
+                const validMovies = movieDetails.filter(movie => movie !== null)
+                console.log(validMovies)
+                setSearchResults(validMovies)
+            } catch (err) {
+                console.error(err)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        fetchMovies()
+    }
+
+    return (
+        <section className='search-section'>
+            <form 
+                className="search-component"
+                onSubmit={handleSearch}>
+                <input 
+                    type="search"
+                    onChange={e => setSearchInput(e.target.value)}
+                    value={searchInput} />
+                <button type="submit" >Search</button>
+            </form>
+
+            <div>
+                {isLoading? 
+                <FadeLoader
+                    loading={isLoading}
+                    size={100}
+                    color="grey"
+                    cssOverride={{
+                        display: 'block',
+                        margin: '100px auto',
+                        borderColor: '#007bff'
+                    }}
+                    aria-label="Loading Spinner"
+                    data-testid="loader"
+                /> :searchResults.map((movie, index) => {
+                    return (<h1 key={index}>{movie.title}</h1>)
+                })
+                }
+            </div>
         </section>
     )
 }
