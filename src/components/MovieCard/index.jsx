2302f91ec7f0b9579b0b3c2f30800355766dfab5
Nihal <nihalmotukuri@gmail.com>
2025-05-20 13:11:57 +0530

    Fourth commit

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,10 +1,18 @@
+import { useNavigate } from 'react-router-dom'
 import './index.css'
 
 const MovieCard = props => {
     const {movie} = props
+    const navigate = useNavigate()
+
+    const onMovieCard = () => {
+        navigate('/movie-details', {
+            state: {movie}
+        })
+    }
 
     return (
-        <div className="movie-card">
+        <div className="movie-card" onClick={onMovieCard}>
             <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt="movie_poster" />
         </div>
     )
--- src/pages/MovieDetails/index.jsx
+++ src/pages/MovieDetails/index.jsx
@@ -1,9 +1,24 @@
+import { useState, Effect, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import './index.css'
 
 const MovieDetails = () => {
+    const location = useLocation()
+    const { movie } = location.state
+
+    const ytUrl = `https://www.youtube.com/embed/${movie.trailerKey}`
+
     return (
         <section>
-
+            <iframe
+                width="560"
+                height="315"
+                src={ytUrl}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>
+            {console.log(movie)}
         </section>
     )
 }
